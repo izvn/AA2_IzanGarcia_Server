c223e011ef535e7acb6d6b5751baa083bec87dd9
izvn <124562397+izvn@users.noreply.github.com>
2026-05-08 17:50:37 +0200

    Game

--- AA2_IzanServer/AA2_IzanServer/main.cpp
+++ AA2_IzanServer/AA2_IzanServer/main.cpp
@@ -2,13 +2,19 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <optional>
 #include <vector>
 #include <cstdlib>
 #include <ctime>
-#include <algorithm>
 
-std::vector<std::string> salasActivas;
+struct Sala {
+    std::string codigo;
+    int jugadores;
+    int turno;
+    int tablero[3][3];
+    int ganador;
+};
+
+std::vector<Sala> salasActivas;
 
 bool registrarUsuario(const std::string& user, const std::string& pass) {
     std::ifstream archivoLectura("usuarios.txt");
@@ -41,14 +47,25 @@ std::string generarCodigo() {
     return codigo;
 }
 
+int comprobarGanador(int t[3][3]) {
+    for (int i = 0; i < 3; i++) {
+        if (t[i][0] != 0 && t[i][0] == t[i][1] && t[i][1] == t[i][2]) return t[i][0];
+        if (t[0][i] != 0 && t[0][i] == t[1][i] && t[1][i] == t[2][i]) return t[0][i];
+    }
+    if (t[0][0] != 0 && t[0][0] == t[1][1] && t[1][1] == t[2][2]) return t[0][0];
+    if (t[0][2] != 0 && t[0][2] == t[1][1] && t[1][1] == t[2][0]) return t[0][2];
+    bool empate = true;
+    for (int i = 0; i < 3; i++) for (int j = 0; j < 3; j++) if (t[i][j] == 0) empate = false;
+    if (empate) return 3;
+    return 0;
+}
+
 int main() {
     srand(static_cast<unsigned>(time(nullptr)));
     std::cout << "=== BOOTSTRAP SERVER INICIADO ===\n";
 
     sf::TcpListener listener;
-    if (listener.listen(50000) != sf::Socket::Status::Done) {
-        return -1;
-    }
+    if (listener.listen(50000) != sf::Socket::Status::Done) return -1;
 
     while (true) {
         sf::TcpSocket client;
@@ -67,19 +84,58 @@ int main() {
                         client.send(packetRespuesta);
                     }
                     else if (type == 3) {
-                        std::string nuevoCodigo = generarCodigo();
-                        salasActivas.push_back(nuevoCodigo);
-                        std::cout << "Nueva sala creada: " << nuevoCodigo << "\n";
-                        packetRespuesta << 1 << nuevoCodigo;
+                        Sala s;
+                        s.codigo = generarCodigo();
+                        s.jugadores = 1;
+                        s.turno = 1;
+                        s.ganador = 0;
+                        for (int i = 0; i < 3; i++) for (int j = 0; j < 3; j++) s.tablero[i][j] = 0;
+                        salasActivas.push_back(s);
+                        packetRespuesta << 1 << s.codigo << 1;
                         client.send(packetRespuesta);
                     }
                     else if (type == 4) {
                         std::string codigo;
                         packetRecibido >> codigo;
-                        bool existe = (std::find(salasActivas.begin(), salasActivas.end(), codigo) != salasActivas.end());
-                        if (existe) std::cout << "Jugador se unio a la sala: " << codigo << "\n";
-                        packetRespuesta << (existe ? 1 : 0);
+                        bool encontrado = false;
+                        for (auto& s : salasActivas) {
+                            if (s.codigo == codigo && s.jugadores == 1) {
+                                s.jugadores = 2;
+                                packetRespuesta << 1 << 2;
+                                client.send(packetRespuesta);
+                                encontrado = true;
+                                break;
+                            }
+                        }
+                        if (!encontrado) { packetRespuesta << 0; client.send(packetRespuesta); }
+                    }
+                    else if (type == 5) {
+                        std::string codigo; int id, x, y;
+                        packetRecibido >> codigo >> id >> x >> y;
+                        bool ok = false;
+                        for (auto& s : salasActivas) {
+                            if (s.codigo == codigo && s.turno == id && s.tablero[x][y] == 0 && s.ganador == 0) {
+                                s.tablero[x][y] = id;
+                                s.ganador = comprobarGanador(s.tablero);
+                                s.turno = (id == 1) ? 2 : 1;
+                                ok = true;
+                                break;
+                            }
+                        }
+                        packetRespuesta << (ok ? 1 : 0);
                         client.send(packetRespuesta);
+                    }
+                    else if (type == 6) {
+                        std::string codigo;
+                        packetRecibido >> codigo;
+                        for (auto& s : salasActivas) {
+                            if (s.codigo == codigo) {
+                                packetRespuesta << s.turno << s.ganador;
+                                for (int i = 0; i < 3; i++) for (int j = 0; j < 3; j++) packetRespuesta << s.tablero[i][j];
+                                client.send(packetRespuesta);
+                                break;
+                            }
+                        }
                     }
                 }
             }
